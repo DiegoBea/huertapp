9657998302d8e0c14363428d74c534aaf789cc57
Diego Bea <diegobeagomez1@gmail.com>
2022-09-16 16:13:31 +0200

    Más cambios

--- lib/ui/input_decorations.dart
+++ lib/ui/input_decorations.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:huertapp/providers/theme_provider.dart';
-import 'package:huertapp/themes/app_theme.dart';
 
 class InputDecorations {
   static InputDecoration authInputDecorations(
